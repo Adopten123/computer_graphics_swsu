3179363f627e5d1d30861864ee45c8b6acffd78e
adopten <alexnadermikhailov0505@gmail.com>
2025-09-06 16:07:41 +0300

    lb1

---
+++ lb1/ImgConverter/build/CMakeFiles/imgconv.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imgconv.dir/link.d"
+  "CMakeFiles/imgconv.dir/main.cpp.o"
+  "CMakeFiles/imgconv.dir/main.cpp.o.d"
+  "imgconv"
+  "imgconv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imgconv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
